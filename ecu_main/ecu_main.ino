333ff84913a0eb570d044c90cec6425672fb8f83
Ethan Budde <61395479+EthanBudde@users.noreply.github.com>
2026-04-29 14:48:21 -0700

    updated to reflect burnin changes, more active data tracking

--- ecu_main/ecu_main.ino
+++ ecu_main/ecu_main.ino
@@ -10,6 +10,9 @@
 #include <Wire.h>
 
 #define SEALEVELPRESSURE_HPA (1013.25)
+#define BMEPIN 33
+#define SCDPIN 27
+#define SGPPIN 12
 
 // sensor Objects
 Adafruit_SCD30 scd30;
@@ -25,10 +28,95 @@ char password[] = "bigswag!";
 
 int count = 0;
 
+// coherence structs, last/current holders
+struct bmeValues{
+  float temp;
+  float pres;
+  float humid;
+  float gasr;
+};
+
+bmeValues bmeCurrent, bmeLast;
+
+struct scdValues{
+  float temp;
+  float humid;
+  float co2;  
+};
+
+scdValues scdCurrent, scdLast;
+
+struct sgpValues{
+  int tvoc;
+  int co2;
+};
+
+sgpValues sgpCurrent, sgpLast;
+
+//getPinMode
+//  checks pin is valid port, returns pin type
+uint32_t getPinMode(uint32_t pin) {
+  uint32_t bit = digitalPinToBitMask(pin);
+  uint32_t port = digitalPinToPort(pin);
+  if (port == NOT_A_PIN) return 0xFF;
+
+  volatile uint32_t *reg = portModeRegister(port);
+  volatile uint32_t *out = portOutputRegister(port);
+
+  if (*reg & bit) {
+    return OUTPUT; // Pin is configured as an output
+  } else {
+    return (*out & bit) ? INPUT_PULLUP : INPUT; // Checks if pull-up is active
+  }
+}
+
+//blinkpin
+  //checks pin valid output
+  //blinks selected pattern at selected pin
+void blinkPin(int pin, int pattern){
+  if(getPinMode(pin) != OUTPUT) {
+    // some error notation
+    return;
+  }
+
+  switch(pattern){
+    case 0:                   // blink confirm - 2 long blinks, for setup
+      digitalWrite(pin, HIGH);
+      delay(500);
+      digitalWrite(pin, LOW);
+      delay(500);
+      digitalWrite(pin, HIGH);
+      delay(500);
+      digitalWrite(pin, LOW);
+      delay(500);
+      break;
+    case 1:                   // enable LED
+      digitalWrite(pin, HIGH);
+      break;
+    case 2:                   // disable LED
+      digitalWrite(pin, LOW);
+      delay(20);
+      break;
+    default:
+      return;
+  }
+}
+
 void setup() {
   // serial comms
+    // serial 
   Serial.begin(115200);
-	while (!Serial) delay(10);	// will pause until serial terminal open
+  
+  // pinmode
+  pinMode(BMEPIN, OUTPUT);
+  pinMode(SCDPIN, OUTPUT);
+  pinMode(SGPPIN, OUTPUT);
+
+  blinkPin(BMEPIN, 1);
+	blinkPin(SCDPIN, 1);
+  blinkPin(SGPPIN, 1);
+
+  //while (!Serial) delay(10);	// will pause until serial terminal open
 	
   // wifi signal chain
 	WiFi.begin(ssid, password);
@@ -50,7 +138,12 @@ void setup() {
     waitForSync();
     myTZ.setLocation(F("America/Los_Angeles"));
   }
+    
   
+  blinkPin(BMEPIN, 2);
+	blinkPin(SCDPIN, 2);
+  blinkPin(SGPPIN, 2);
+
   // bme wakeup
   if (!bme.begin()) {
 		Serial.println("Could not find a valid BME680 sensor, check wiring!");
@@ -63,7 +156,10 @@ void setup() {
 	bme.setPressureOversampling(BME680_OS_4X);	
 	bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
 	bme.setGasHeater(320, 150); // 320*C for 150 ms
-  
+
+  // bme blink confirm
+  blinkPin(BMEPIN, 0);
+
   // sgp wakeup
   if (! sgp.begin()){
     Serial.println("SGP sensor not found :(");
@@ -73,6 +169,9 @@ void setup() {
   Serial.print(sgp.serialnumber[0], HEX);
   Serial.print(sgp.serialnumber[1], HEX);
   Serial.println(sgp.serialnumber[2], HEX);
+
+  //sgp blink confirm
+  blinkPin(SGPPIN, 0);
 
   // scd wakeup
   if (!scd30.begin()) {
@@ -109,45 +208,112 @@ void setup() {
     Serial.print("Self calibration disabled");
   }
   Serial.println("\n\n");
+
+  // scd blink confirm
+  blinkPin(SCDPIN, 0);
 }
 
 void loop() {
   // timestamp
   if(count == 0){Serial.println(myTZ.dateTime("H:i:s.v"));}
   
-  // bme print block
-  // we assume the BME is always gonna give us something, so its at the top
-  Serial.println("[BME BLOCK]");
-  Serial.print(bme.temperature); Serial.println("[degC]");
-  Serial.print(bme.pressure / 100.0); Serial.println("[hPaPres]");
-  Serial.print(bme.humidity); Serial.println("[%humid]");
-  Serial.print(bme.gas_resistance / 1000.0); Serial.println("[KOhmsGasR]");
-  Serial.println();
+  bmeRead();
+  scdRead();
+  sgpRead();
 
+  Serial.println();  Serial.println();  
+  delay(50);
+}
+
+void bmeRead(){
+  // bme check valid
+  if (! bme.performReading()) {
+		return;
+	} else {
+    // bme oplight on
+    blinkPin(BMEPIN, 1);
+
+    bmeCurrent.temp = bme.temperature;
+    bmeCurrent.pres = bme.pressure / 100.0;
+    bmeCurrent.humid = bme.humidity;
+    bmeCurrent.gasr = bme.gas_resistance / 1000.0;
+  }
+
+  if((bmeCurrent.temp == bmeLast.temp) && (bmeCurrent.pres == bmeLast.pres) && (bmeCurrent.humid == bmeLast.humid) && (bmeCurrent.gasr == bmeLast.gasr)){
+    // bme oplight off
+    blinkPin(BMEPIN, 2);
+    return; 
+  }else{ 
+    
+    // bme print block
+    Serial.println("[BME BLOCK]");
+    Serial.print(bme.temperature); Serial.println("[degC]");
+    Serial.print(bme.pressure / 100.0); Serial.println("[hPaPres]");
+    Serial.print(bme.humidity); Serial.println("[%humid]");
+    Serial.print(bme.gas_resistance / 1000.0); Serial.println("[KOhmsGasR]");
+    Serial.println();
+
+    bmeLast = bmeCurrent;
+  }
+  // bme oplight off
+    blinkPin(BMEPIN, 2);
+}
+
+void scdRead(){
   //ping scd for valid data reading
   if (scd30.dataReady()) {
     if (!scd30.read()){ 
       return; 
-    } if (! bme.performReading()) {
-		return;
-	  }
-    // scd print block
-    Serial.println("[SCD BLOCK]");
-    Serial.print(scd30.temperature); Serial.println("[degC]");
-    Serial.print(scd30.relative_humidity); Serial.println("[%humid]");
-	  Serial.print(scd30.CO2, 3); Serial.println("[ppmCO2]");
-    Serial.println();
-  }
+    } else {
+      // scd oplight on
+      blinkPin(SCDPIN, 1);
 
-  
+      scdCurrent.temp = scd30.temperature;
+      scdCurrent.humid = scd30.relative_humidity;
+      scdCurrent.co2 = scd30.CO2;
+
+      if((scdCurrent.temp == scdLast.temp)&&(scdCurrent.humid == scdLast.humid)&&(scdCurrent.co2 == scdLast.co2)){
+        //sgp oplight off
+        blinkPin(SCDPIN, 2);
+        return;
+      } else {
+        // scd print block
+        Serial.println("[SCD BLOCK]");
+        Serial.print(scd30.temperature); Serial.println("[degC]");
+        Serial.print(scd30.relative_humidity); Serial.println("[%humid]");
+	      Serial.print(scd30.CO2, 3); Serial.println("[ppmCO2]");
+        Serial.println();
+        scdLast = scdCurrent;
+      }
+    //sgp oplight off
+    blinkPin(SCDPIN, 2);
+    }
+  }  
+}
+
+void sgpRead(){
+  // sgp data valid
   if (! sgp.IAQmeasure()) {
     return;
   } else {
-	// sgp print block  
+    // sgp oplight on
+    blinkPin(SGPPIN, 1);
+
+    sgpCurrent.tvoc = sgp.TVOC;
+    sgpCurrent.co2 = sgp.eCO2;
+  }
+
+  if((sgpCurrent.tvoc == sgpLast.tvoc)&&(sgpCurrent.tvoc == sgpLast.tvoc)){
+    //sgp oplight off
+    blinkPin(SGPPIN, 2);
+    return;
+  } else {
+    // sgp print block  
     Serial.println("[SGP BLOCK]");
     Serial.print(sgp.TVOC); Serial.println("[ppbTVOC]");
     Serial.print(sgp.eCO2); Serial.println("[ppmCO2]");
+    sgpLast = sgpCurrent;
   }
-  Serial.println();  Serial.println();  
-  delay(900);
+  //sgp oplight off
+  blinkPin(SGPPIN, 2);
 }
